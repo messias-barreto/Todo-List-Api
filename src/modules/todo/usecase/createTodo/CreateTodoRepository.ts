99edd96fa36b6c71171aa86557f8b3877e14e946
messiasbarreto <messias.barreto@unisuam.edu.br>
2023-04-12 00:56:36 -0300

    feat: Shold not be able create a new Todo if Project does not Exists

--- src/modules/todo/usecase/createTodo/CreateTodoRepository.ts
+++ src/modules/todo/usecase/createTodo/CreateTodoRepository.ts
@@ -1,5 +1,7 @@
+import { AppErrors } from "../../../../shared/errors/AppErrors";
 import { IprojectRepository } from "../../../project/interfaces/IProjectRepository";
 import { ITodoDTO } from "../../dtos/ITodoDTO";
+import { Todo } from "../../infra/typeorm/entities/Todo";
 import { ITodoRepository } from "../../interfaces/ITodoRepository";
 
 
@@ -8,7 +10,12 @@ class CreateTodoRepository {
         private todoRepository: ITodoRepository,
         private projectRepository: IprojectRepository
     ){}
-    async execute({ title, description, status, project_id }: ITodoDTO){
+    async execute({ title, description, status, project_id }: ITodoDTO):Promise<Todo> {
+        const projectAlreadyExists = await this.projectRepository.findProjectsById(project_id);
+        if(!projectAlreadyExists) {
+            throw new AppErrors("Project does not Exists!");
+        }
+
         const todo = await this.todoRepository.create({
             title, 
             description,
