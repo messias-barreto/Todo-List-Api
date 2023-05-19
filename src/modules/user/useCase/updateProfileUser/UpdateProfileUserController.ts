08d0f423d7be06924a18f51149fc8f8800c1468e
messiasbarreto <messias.barreto@unisuam.edu.br>
2023-05-18 23:16:29 -0300

    test: CI test

--- src/modules/user/useCase/updateProfileUser/UpdateProfileUserController.ts
+++ src/modules/user/useCase/updateProfileUser/UpdateProfileUserController.ts
@@ -10,7 +10,7 @@ class UpdateProfileUserController {
         const updateProfileUserUseCase = container.resolve(UpdateProfileUserUseCase);
         await updateProfileUserUseCase.execute({ name, login, email, id });
         
-        return response.json({ message: "Dados do Usuário Atualizado!" });
+        return response.json({ message: "Dados do Usuário Atualizados!" });
     }
 }
 
